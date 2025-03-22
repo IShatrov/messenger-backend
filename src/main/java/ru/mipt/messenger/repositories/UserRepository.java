03aa930fb6ea4be61cc76481354370721c40170b
IShatrov <shatrov2004@mail.ru>
2025-03-22 23:51:38 +0300

    Reorganised imports in UserRepository.java

--- src/main/java/ru/mipt/messenger/repositories/UserRepository.java
+++ src/main/java/ru/mipt/messenger/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package ru.mipt.messenger.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.mipt.messenger.models.User;
 
 import java.util.List;
+
+import ru.mipt.messenger.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findUsersByFirstname(String firstname);
