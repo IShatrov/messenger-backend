451ca34592fc3d48a38db2037645f09e19a9fb1e
IShatrov <shatrov2004@mail.ru>
2025-03-23 00:17:04 +0300

    Corrected mistakes in comments

--- src/main/java/ru/mipt/messenger/controllers/UserController.java
+++ src/main/java/ru/mipt/messenger/controllers/UserController.java
@@ -29,14 +29,14 @@ public class UserController {
      * It is not required to explicitly assign userId to null in the JSON since it defaults to null.
      * 2) nickname, firstname, phone, email and password are not null, not blank, have valid length and match patterns
      * if required.
-     * 3) nickname, firstname, phone and email are unique in the database.
+     * 3) nickname, phone and email are unique in the database.
      * 4) If registrationDttm is not null, it matches pattern "yyyy-MM-dd'T'HH:mm:ss.SSSZ". If registrationDttm is null,
      * it defaults to current UTC time.
      * @see <a href="https://docs.spring.io/spring-framework/docs/3.2.4.RELEASE_to_4.0.0.M3/Spring%20Framework%203.2.4.RELEASE/org/springframework/format/annotation/DateTimeFormat.ISO.html">Spring docs</a>
      * 5) dateOfBirth is not null and matches pattern "yyyy-MM-dd".
      * 6) If secondName is not null, it has valid length.
      * 7) If status is not null, it is either "Banned" or "Active". If status is null, it defaults to "Active".
-     * 8) If role is not null, it is either "Admin" or "User". If role is null. it defaults to "User".
+     * 8) If role is not null, it is either "Admin" or "User". If role is null, it defaults to "User".
      * 9) If profilePictureLink is not null, it has valid length.
      * @param user User to create
      * @throws DataIntegrityViolationException if userId is not null or other database error occurs.
