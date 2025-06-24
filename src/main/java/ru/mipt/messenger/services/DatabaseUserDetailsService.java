631dfb4372ac9f6789ffc59d99f676de368c8c5f
alisavi <alisa.victorova@outlook.com>
2025-06-24 22:51:54 +0300

    working

--- src/main/java/ru/mipt/messenger/services/DatabaseUserDetailsService.java
+++ src/main/java/ru/mipt/messenger/services/DatabaseUserDetailsService.java
@@ -1,6 +1,8 @@
 package ru.mipt.messenger.services;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
